f16f23a61412ebe70d6e5110a5c3019f815476ed
mong3125 <mong3125@naver.com>
2023-08-16 10:45:42 +0900

    BOJ_2018 (수들의 합 5)

---
+++ src/BOJ_2018.java
@@ -0,0 +1,38 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2018 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        long count = 0;
+
+        int start = 1;
+        int end = 1;
+
+        int sum  = 1;
+        while (start <= N) {
+            if (sum == N) {
+                count++;
+
+                sum -= start;
+                start++;
+
+                end++;
+                sum += end;
+            } else if (sum < N) {
+                end++;
+                sum += end;
+            } else {
+                sum -= start;
+                start++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
