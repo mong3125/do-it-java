35f2379f91452083f249f3fb739c1773b0df1c11
mong3125 <mong3125@naver.com>
2024-05-31 19:24:05 +0900

    boj 바이러스 java solution

--- src/BFS/BOJ31791_바이러스.java
+++ src/BFS/BOJ31791_바이러스.java
@@ -3,20 +3,15 @@ package BFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Objects;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ31791_바이러스 {
     static int N, M;    // 행, 열 크기
     static int Tg, Tb, X, B;    // 전파되는 시간, 건물에서 전파되는 시간, 바이러스 개수, 건물의 개수
-    static char[][] board;
 
+    static int[][] board;
     static boolean[][] visited;
-    static ArrayList<Point> virus = new ArrayList<>();
-    static ArrayList<Point> next = new ArrayList<>();
-    static HashMap<Point, Integer> building = new HashMap<>();
+    static PriorityQueue<int[]> pq = new PriorityQueue<>(((o1, o2) -> o1[2] - o2[2]));
 
     static int[][] direction = new int[][]{
             {1, 0},
@@ -27,12 +22,22 @@ public class BOJ31791_바이러스 {
 
     public static void main(String[] args) throws IOException {
         // 입력
+        input();
+
+        // 풀이
+        bfs();
+
+        // 출력
+        output();
+    }
+
+    private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        board = new char[N][M];
+        board = new int[N][M];
         visited = new boolean[N][M];
 
         st = new StringTokenizer(br.readLine());
@@ -44,101 +49,62 @@ public class BOJ31791_바이러스 {
         for (int i = 0; i < N; i++) {
             String row = br.readLine();
             for (int j = 0; j < M; j++) {
-                board[i][j] = row.charAt(j);
-                if (board[i][j] == '*') virus.add(new Point(i, j));
-                else if (board[i][j] == '#') building.put(new Point(i, j), 0);
-            }
-        }
-
-        // 풀이
-        for (int i = 0; i < Tg; i++) {
-            bfs();
-        }
-
-        // 출력
-        boolean isAnySafe = false;
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (board[i][j] != '*') {
-                    isAnySafe = true;
-                    System.out.printf("%d %d\n", i, j);
+                switch (row.charAt(j)) {
+                    case '*':
+                        board[i][j] = -Tg - 1;
+                        visited[i][j] = true;
+                        pq.add(new int[] {j, i, -Tg});
+                        break;
+                    case '#':
+                        board[i][j] = Tb;
+                        break;
+                    default:
+                        board[i][j] = 0;
                 }
             }
         }
-
-        if (!isAnySafe) System.out.println(-1);
     }
 
     public static void bfs() {
-        // 빌딩 시간 쌓기
-        for (Point p : building.keySet()) {
-            int count = building.get(p);
+        while (!pq.isEmpty()) {
+            int[] now = pq.poll();
 
-            // 지연시간 이상 시간이 지났다면 감염
-            if (count >= Tg) {
-                board[p.y][p.x] = '*';
-                next.add(p);
-                building.remove(p);
-            } else if (count > 0) {
-                building.put(p, building.get(p) + 1);
-            }
-        }
+            if (now[2] >= 0) continue;
 
-        for (Point virusPoint : virus) {
             for (int[] d : direction) {
-                int y = virusPoint.y + d[0];
-                int x = virusPoint.x + d[1];
-                Point nowPoint = new Point(y, x);
+                int x = now[0] + d[0];
+                int y = now[1] + d[1];
 
                 if (isInRange(y, x)) {
-                    // 방문한 위치라면 패스
                     if (visited[y][x]) continue;
 
-                    // 방문 처리
                     visited[y][x] = true;
 
-                    // 만약 감염되지 않은 장소라면 감염시키기
-                    if (board[y][x] == '.') {
-                        board[y][x] = '*';
-                        next.add(nowPoint);
-                    // 빌딩이라면 감염 시작
-                    } else if (board[y][x] == '#') {
-                        if (building.get(nowPoint) == 0) {
-                            building.put(nowPoint, 1);
-                        }
-                    }
+                    board[y][x] = board[y][x] + now[2];
+                    pq.add(new int[] {x, y, board[y][x] + 1});
+                }
+            }
+        }
+    }
+
+    private static void output() {
+        boolean isAnySafe = false;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (board[i][j] >= 0) {
+                    isAnySafe = true;
+                    sb.append(String.format("%d %d\n", i+1, j+1));
                 }
             }
         }
 
-        virus.clear();
-        virus.addAll(next);
-        next.clear();
+        if (isAnySafe) System.out.println(sb.toString());
+        else System.out.println(-1);
     }
 
     public static boolean isInRange(int y, int x) {
         return x >= 0 && x < M && y >= 0 && y < N;
-    }
-
-    static class Point {
-        private int x, y;
-        Point(int y, int x) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Point p = (Point) o;
-            return x == p.x && y == p.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
     }
 }
